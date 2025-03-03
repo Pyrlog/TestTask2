e5df4f34cf723571f8bd26e1e4d63d42420cca4a
Margarita Pirlog <kuzmenkova.margarita@gmail.com>
2025-03-03 23:22:18 +0200

    Added test for post to search product

--- test_website.py
+++ test_website.py
@@ -21,8 +21,18 @@ def test_get_all_brands_list():
     assert json_response.get('responseCode') == 200
     assert type(json_response.get('brands')) == list
 
+
 def test_put_to_all_brands_list():
     response = requests.put("https://automationexercise.com/api/brandsList")
     json_response = response.json()
     assert json_response.get('responseCode') == 405
     assert json_response.get('message') == 'This request method is not supported.'
+
+
+def test_post_to_search_product():
+    response = requests.post("https://automationexercise.com/api/searchProduct", data = {"search_product": "top"})
+    json_response = response.json()
+    assert json_response.get('responseCode') == 200
+    assert type(json_response.get('products')) == list
+
+
