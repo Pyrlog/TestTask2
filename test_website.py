02c7512b06e22540a265677191acffb57e9a5848
Margarita Pirlog <kuzmenkova.margarita@gmail.com>
2025-03-04 12:38:43 +0200

    Added test for post to verify login with invalid details

--- test_website.py
+++ test_website.py
@@ -63,6 +63,13 @@ def test_delete_to_verify_login():
     assert json_response.get('message') == 'This request method is not supported.'
 
 
+def test_post_to_verify_login_with_invalid_details():
+    response = requests.post("https://automationexercise.com/api/verifyLogin", data = {"email": "johndoe765@@gmail.com", "password": "password"})
+    json_response = response.json()
+    assert json_response.get('responseCode') == 404
+    assert json_response.get('message') == 'User not found!'
+
+
 #def test_post_to_create_register_user_acount():
 #    response = requests.post("https://automationexercise.com/api/createAccount", data = {"name": "John", "email": "johndoe765@gmail.com", "password": "password", "title": "Mr", "birth_date": "01", "birth_month": "01", "birth_year": "1991", "firstname": "John", "lastname": "Doe", "company": "Best_company", "address1": "Pushkina_5", "address2": "7", "country": "USA", "zipcode": "1111", "state": "Texas", "city": "Houston", "mobile_number": "123456"})
 #    json_response = response.json()
